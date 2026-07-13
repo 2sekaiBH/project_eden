baa6bd0edaacc021a5e836aac3471af0f8c9a50b
라희 <huhu4886@naver.com>
2026-07-13 17:27:33 +0900

    컴퓨터와 연동용

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -6,10 +6,10 @@ public class Inventory : MonoBehaviour
 
 
     //처음 인벤토리 슬롯 9개를 로딩함 -> 아이템을 담을 준비
-    public void awake()
+    public void Awake()
     {
-        for (int i=0; i < slots.Length; i++)
-        {   
+        for (int i = 0; i < slots.Length; i++)
+        {
             //만들어 뒀던 인벤토리 슬롯 객체를 붙여줌!
             slots[i] = new InventorySlot();
         }
@@ -20,18 +20,18 @@ public class Inventory : MonoBehaviour
     //아이템이 인벤토리에 존재하는지 확인하는 함수
     public bool HasItem(ItemData item)
     {
-        foreach(InventorySlot slot in slots)
+        foreach (InventorySlot slot in slots)
         {
             if (slot.item == item)
             {
                 Debug.Log("아이템 있음");
                 return true;
+            }
         }
-    }
 
         return false;
- 
-}
+
+    }
 
     //아이템을 인벤토리에 추가하는 함수
     public void AddItem(ItemData item)
@@ -46,3 +46,4 @@ public class Inventory : MonoBehaviour
             }
         }
     }
+}
