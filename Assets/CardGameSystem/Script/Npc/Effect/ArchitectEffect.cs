f690bbc3e20f7dd6d4033d297d40037611fdea8b
zipgale <gksql0721@gmail.com>
2026-07-27 20:49:35 +0900

    [Feat(윤한비)] 아키텍트 npc 효과 구현
    - 적 턴 스킵 효과 구현
    - Opponenet Actor script에 이번 턴 select을 스킵하는 함수 작성
    - ProfileUpdator에 비활성화 상태를 UI에 반영하는 함수 작성

---
+++ Assets/CardGameSystem/Script/Npc/Effect/ArchitectEffect.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using UnityEngine;
+
+/// <summary>
+/// 아키텍트 npc effect - 적 턴 스킵
+/// </summary>
+/// <param name="context"></param>
+[CreateAssetMenu(fileName = "ArchitectEffect", menuName = "Scriptable Objects/CardSystem/NpcEffect/ArchitectEffect")]
+public class ArchitectEffect : NpcEffect
+{
+    public override void Apply(NpcContext context)
+    {
+        var opponent = context.opponentActor;
+        opponent.SetActiveOnCurrentTurn(false);
+
+        void Handler(int _)
+        {   opponent.SetActiveOnCurrentTurn(true);
+            TurnFlowManager.OnTurnEnd -= Handler;
+        }
+        TurnFlowManager.OnTurnEnd += Handler;
+    }
+}
---
+++ Assets/CardGameSystem/Script/Npc/Effect/KainEffect.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "KainEffect", menuName = "Scriptable Objects/KainEffect")]
+public class KainEffect : NpcEffect
+{
+    public override void Apply(NpcContext context)
+    {
+        
+    }
+}
