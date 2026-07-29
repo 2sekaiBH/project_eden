1cb9d2db547f2eca4c42193a2e11a2dcde25bc94
La-hee <haha4886@naver.com>
2026-07-30 01:59:20 +0900

    Reload 카드 시스템 구현

--- Assets/CardGameSystem/Script/Card/CardEffect/CardDrawEffect.cs
+++ Assets/CardGameSystem/Script/Card/CardEffect/CardDrawEffect.cs
@@ -14,13 +14,11 @@ public class CardDrawEffect : CardEffectData
         Debug.Log($"뽑기전 카드 : {context.caster.hand.Count}");
         var card = DeckManager.Instance.DrawExtrCard(1)[0]; //덱매니저 함수에서 카드를 한 장 뽑아옴
         Debug.Log($"뽑은 카드 : {card}");
-        context.caster.hand.Add(card); //카드를  손패에 추가
-        Debug.Log($"손패 개수 : {context.caster.hand.Count}");
 
         //플레이어일 경우 손패 UI 갱신
         if(context.caster is PlayerActor player)
         {
-            player.AddExtraCard(card);
+            player.SetHand(card);
         }
         Debug.Log($"{context.caster}: {count} get");
     }
