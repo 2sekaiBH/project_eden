3ccd1b735a0ae08860749e5a0bd3de57fbe84b42
zipgale <gksql0721@gmail.com>
2026-08-05 18:13:40 +0900

    [Feat(윤한비)] 사운드 매니저 생성

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SoundManager.Instance.PlayBGM(EBgm.Dialogue_399);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GameManager/SoundManager.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EBgm
+{
+    Dialogue_67 = 0,
+    Dialogue_218,
+    Dialogue_399,
+    Dialogue_404,
+    CardGame,
+    CardGame_404,
+    Ending_Arch,
+    Ending_Eve,
+    Ending_Noa,
+    Neon_Static,
+    Prologue
+}
+
+public enum ESfx
+{
+
+}
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance;
+    public static SoundManager Instance => instance;
+
+    private Dictionary<EBgm, AudioClip> bgmDict = new Dictionary<EBgm, AudioClip>();
+    private Dictionary<ESfx, AudioClip> sfxDict = new Dictionary<ESfx, AudioClip>();
+
+    [Header("Reference")]
+    [SerializeField] private AudioSource sfxPlayer; // SFX 재생용 AudioSource
+    [SerializeField] private AudioSource bgmPlayer; // BGM 재생용 AudioSource
+
+    [Header("Audio Clips")]
+    [SerializeField] private AudioClip[] bgmClips;
+    [SerializeField] private AudioClip[] sfxClips;
+
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            // DontDestroyOnLoad(gameObject);
+        }
+
+        Init();
+    }
+
+    private void Init()
+    {
+        // bgm dictionary 초기화
+        for (int i = 0; i < bgmClips.Length; i++)
+        {
+            bgmDict[(EBgm)i] = bgmClips[i];
+        }
+
+        // sfx dictionary 초기화
+        for (int i = 0; i < sfxClips.Length; i++)
+        {
+            sfxDict[(ESfx)i] = sfxClips[i];
+        }
+    }
+    
+    public void PlayBGM(EBgm bgmType)
+    {
+        if(bgmDict.TryGetValue(bgmType, out var clip))
+        {
+            bgmPlayer.clip = clip;
+            bgmPlayer.Play();
+        }
+        else
+        {
+            Debug.LogWarning("Bgm not found in Dictionary!");
+        }
+    }
+
+    public void StopBgm()
+    {
+        bgmPlayer.Stop();
+    }
+
+    public void PlaySFX(ESfx sfxType)
+    {
+        if(sfxDict.TryGetValue(sfxType, out var clip))
+        {
+            sfxPlayer.PlayOneShot(clip);
+        }
+        else
+        {
+            Debug.LogWarning("SFX not found in Dictionary");
+        }
+    }
+
+}
