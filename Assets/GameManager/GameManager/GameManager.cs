e7730f73eff523641c298b6f7b15e616797413e3
zipgale <gksql0721@gmail.com>
2026-08-06 17:50:10 +0900

    [Feat(윤한비)] 설정창 키 맵핑 기능 구현
    - 전역 키 바인딩 데이터를 관리하는 KeyManager 생성
    - 설정창에서 해당 키 설정 버튼 누르고 -> 키보드 입력 시 중복 검사 후 새로운 키로 변경
    - 새로운 키 정보를 KeyManager에 등록

--- Assets/GameManager/GameManager/GameManager.cs
+++ Assets/GameManager/GameManager/GameManager.cs
@@ -26,16 +26,13 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        SoundManager.Instance.PlayBGM(EBgm.Dialogue_399);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.G))
-        {
-            SoundManager.Instance.PlayBGM(EBgm.Dialogue_399);
-        }
+        
     }
 
     public void SetCurrentScene(string newScene)
