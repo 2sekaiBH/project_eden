bf36eaa2f4002ae56941b7c71d6634ff12d76738
zipgale <gksql0721@gmail.com>
2026-08-07 17:53:42 +0900

    [Feat(윤한비)] 플레이어 효과음 재생 트리거 구현

---
+++ Assets/PlayerBehavior/Script/Player/PlayerSFXController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerSFXController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 발소리 효과음 재생 - 애니메이션 이벤트에서 호출
+    /// </summary>
+    public void PlayFootStep()
+    {
+        
+    }
+
+    /// <summary>
+    /// 점프 효과음 재생
+    /// </summary>
+    public void PlayJump()
+    {
+        
+    }
+
+    /// <summary>
+    /// 슬라이드 효과음 재생
+    /// </summary>
+    public void PlaySlide()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
