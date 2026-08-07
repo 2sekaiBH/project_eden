a2c12e5bc7080e9c14a429cb79afcb170894d092
github <pulple.cheey@daum.net>
2026-08-08 03:03:56 +0900

    아이템 최종 정리

--- Assets/Scripts/InventoryUI.cs
+++ Assets/Scripts/InventoryUI.cs
@@ -20,6 +20,7 @@ public class InventoryUI : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
