19c4ab960c7a963cef6bede6bff11bd4f333fdb6
github <pulple.cheey@daum.net>
2026-08-09 00:46:11 +0900

    최종!

--- Assets/SettingsWIndow/SettingsController.cs
+++ Assets/SettingsWIndow/SettingsController.cs
@@ -70,4 +70,15 @@ public class SettingsController : MonoBehaviour
         }
     }
 
+    public void OnQuitButtonClicked()
+    {
+#if UNITY_EDITOR
+        // 유니티 에디터 실행 중일 때는 플레이 모드를 종료
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 실제 빌드된 게임(exe 등)에서는 게임 창을 완전히 종료
+        Application.Quit();
+#endif
+    }
+
 }
